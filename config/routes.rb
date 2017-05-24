025ff114f1a3d611a7e10cd903fc265146ca5167
tnepper <Talia@MacBook-2.local>
2017-05-24 17:39:29 -0500

    Implemented initial view code for audit logs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  resources :audit_logs
+  resources :audit_logs, except: [:new, :edit, :destroy]
+
   namespace :admin do
     resources :users
     resources :posts
