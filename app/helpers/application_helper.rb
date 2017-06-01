2ae22271e025641c66cd992a4d90930d2368e119
tnepper <Talia@MacBook-2.local>
2017-06-01 14:39:10 -0500

    Updated nav to remove components not needed for admins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,20 @@ module ApplicationHelper
     def admin_types
         ['AdminUser']
     end
-    
+
     def active?(path)
         "active" if current_page?(path)
     end
+
+    def employee?
+        current_user.type == 'Employee'
+    end
+
+    def admin?
+        admin_types.include?(current_user.type)
+    end
+
+
     def status_label status
         status_span_generator status
     end
